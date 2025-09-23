69b4356ad8dbf8fb05a740882f8392811e207bb8
Alan Saji <rockstaralansaji@gmail.com>
2025-09-23 15:34:17 +0530

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import { useTRPC } from "@/trpc/client"
 
 const Page = () =>{
+  const trpc = useTRPC();
+  trpc.hello.queryOptions({
+    text:"hello"
+  })
   return(
     <div >
       <Button>
