83e714f4f250740e4c6bcb192ced6acef75bb111
Alan Saji <rockstaralansaji@gmail.com>
2025-11-02 11:37:40 +0530

    e2b sandbox integration

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,11 +1,17 @@
 import {   openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
+import {Sandbox} from "@e2b/code-interpreter";
+import { getSandBox } from "./utils";
 
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "event/helloWorld" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandBoxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("silver-nextjs");
+      return sandbox.sandboxId;
+    })
     const summarizer = createAgent({
       name: "summarizer",
       system: "You are an expert summarizer.  You summarize in two words",
@@ -15,9 +21,13 @@ export const helloWorld = inngest.createFunction(
     const {output } = await summarizer.run(
       `Summarize the following text: ${event.data.value}`  
     )
-    console.log(output);
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandBox(sandBoxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    })
     
     
-    return { output };
+    return { output, sandboxUrl };
   },
 );
