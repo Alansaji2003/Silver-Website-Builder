9f3b33f6882f7ac7fe0e7bdc74fda9b01e050b01
Alan Saji <rockstaralansaji@gmail.com>
2025-11-30 21:44:05 +0530

    feat: add project view with message container, project header, and global theme/TRPC setup.

---
+++ src/modules/projects/ui/components/project-header.tsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { ChevronDownIcon, ChevronLeftIcon, EditIcon, SunMoonIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuPortal,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger,
+    DropdownMenuSeparator,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useState } from "react";
+
+
+interface Props {
+    projectId: string;
+}
+
+export const ProjectHeader = ({ projectId }: Props) => {
+    const trpc = useTRPC();
+    const { data: project } = useSuspenseQuery(trpc.projects.getOne.queryOptions({
+        id: projectId
+    }));
+    const { theme, setTheme } = useTheme();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    return (
+        <header className="flex items-center justify-between p-2">
+
+            <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" size="sm" className="focus-visible:ring-0 hover:bg-transparent hover:opacity-75 transition-opacity pl-2!">
+                        <Image src="/Silverlogo.png" alt="Silver" width={18} height={18} />
+                        <span className="text-sm font-medium">{project.name}</span>
+                        <ChevronDownIcon />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="start">
+                    <DropdownMenuItem asChild>
+                        <Link href="/"><ChevronLeftIcon /> <span>
+                            Go to dashboard
+                        </span></Link>
+                    </DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuSub>
+                        <DropdownMenuSubTrigger className="gap-2">
+                            <SunMoonIcon className="size-4 text-muted-foreground" /> <span>Change theme</span>
+                        </DropdownMenuSubTrigger>
+                        <DropdownMenuPortal>
+                            <DropdownMenuSubContent>
+                                <DropdownMenuRadioGroup value={theme} onValueChange={setTheme}>
+                                    <DropdownMenuRadioItem value="light"><span>
+                                        Light
+                                    </span></DropdownMenuRadioItem>
+                                    <DropdownMenuRadioItem value="dark"><span>Dark
+                                    </span></DropdownMenuRadioItem>
+                                    <DropdownMenuRadioItem value="system"><span>System
+                                    </span></DropdownMenuRadioItem>
+                                </DropdownMenuRadioGroup>
+                            </DropdownMenuSubContent>
+                        </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </header>
+    )
+}
--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,21 +1,22 @@
 "use client";
 
 import { useTRPC } from "@/trpc/client";
-import { useSuspenseQuery } from "@tanstack/react-query";
 import {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup
 } from "@/components/ui/resizable"
 import { MessagesContainer } from "../components/messages-container";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+import { Fragment } from "@/generated/prisma";
+import { ProjectHeader } from "../components/project-header";
 
 interface Props {
     projectId: string;
 };
 export const ProjectView = ({ projectId }: Props) => {
     const trpc = useTRPC();
-
+    const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
 
     return (
         <div className="h-screen">
@@ -24,8 +25,14 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={35}
                     minSize={20}
                     className="flex flex-col min-h-0">
+                    <Suspense fallback={<div>Loading Project...</div>}>
+                        <ProjectHeader projectId={projectId} />
+                    </Suspense>
                     <Suspense fallback={<div>Loading Messages...</div>}>
-                        <MessagesContainer projectId={projectId} />
+                        <MessagesContainer projectId={projectId}
+                            activeFragment={activeFragment}
+                            setActiveFragment={setActiveFragment}
+                        />
                     </Suspense>
 
                 </ResizablePanel>
