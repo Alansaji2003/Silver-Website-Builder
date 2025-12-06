26e04f5cf14dfdfc2450dba5e4d8e515b92497a9
Alan Saji <rockstaralansaji@gmail.com>
2025-12-06 15:36:26 +0530

    e2b bug fix in config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["e2b", "@e2b/code-interpreter"],
+
 };
 
 export default nextConfig;
