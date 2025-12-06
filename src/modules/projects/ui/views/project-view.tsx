fd412caa2f82eb8da8e0acc7ee7d47b0db9cd128
Alan Saji <rockstaralansaji@gmail.com>
2025-12-06 20:20:38 +0530

    test

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -42,7 +42,7 @@ export const ProjectView = ({ projectId }: Props) => {
 
     if (isMobile) {
         return (
-            <div className="h-screen flex flex-col relative pt-0.1">
+            <div className="h-screen flex flex-col relative pt-10">
                 {mobileView === "chat" ? (
                     <div className="flex flex-col h-full">
                         <ErrorBoundary fallback={<div>Failed to load project details</div>}>
